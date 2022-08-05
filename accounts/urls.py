71603e386b6e189321a11392e37b5ba9af0d6702
oluwasegun31 <oluwasegun1412@gmail.com>
2022-08-05 18:49:15 +0100

    authsystem

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import Register, Login, Testdash
+from .views import Register, Login, logoutuser, Testdash
+app_name = "accounts"
+
 
 urlpatterns = [
     path('register/', Register, name="register-page"),
     path('login/', Login, name="login-page"),
+    path('logout/', logoutuser, name="logout" ),
     # test run delete later
     path('test/', Testdash, name="test-page")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,5 @@
-from cmath import log
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from nameparser import HumanName
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -11,17 +9,17 @@ from django.contrib.auth.decorators import login_required
 def Register(request):
     if request.method == "POST":
         fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
         email = request.POST.get('email')
         password = request.POST.get('pswrd')
 
         new_user = User.objects.create_user(username = email, email = email, password = password)
 
-        parsed_name = HumanName(fname) 
-        new_user.first_name = parsed_name.first
-        new_user.last_name = parsed_name.last
+        new_user.first_name = fname
+        new_user.last_name = lname
 
         new_user.save()
-        return redirect('login-page')
+        return redirect('accounts:login-page')
 
     return render(request, 'accounts/sign up.html', {})
 
@@ -33,15 +31,18 @@ def Login(request):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect('test-page')
+            return redirect('accounts:test-page')
         else:
-            messages.success(request, 'Email Address and Password  not correct')
+            messages.success(request, 'Email Address and Password do not exist')
 
 
     return render(request, 'accounts/sign in.html', {})
 
-#test run delete later
+def logoutuser(request):
+    logout(request)
+    return redirect('accounts:login-page')
 
-@login_required (login_url='login-page')
+#test run delete when dashboard is ready
+@login_required (login_url='accounts:login-page')
 def Testdash(request):
     return render(request, 'accounts/dbord.html', {})
