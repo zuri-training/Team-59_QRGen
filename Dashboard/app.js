64ec0d870ab11dc876a38d82dc47897e27789be7
ScottDeev <99127718+ScottDeev@users.noreply.github.com>
2022-08-06 20:48:52 +0100

    Check comment with 'NEW'
    Comment with NEW in front are the latest update.
    App.js file and css files too where updated

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -173,3 +173,86 @@ document.getElementById('imgbtn').addEventListener('click', function(e){
     e.preventDefault()
     document.getElementById('imgfile').click()
 })
+
+// DOWNLOAD
+const downloadOptionBtn = document.querySelector('.buttons .download_btn_option');
+const downloadOptions = document.querySelector('.download_options');
+const cancleIcon = document.querySelector('.cancel');
+const downloadBtn = document.querySelectorAll('.download_btn');
+// console.log(downloadOptionBtn);
+console.log(downloadOptions);
+
+downloadOptionBtn.addEventListener('click', function(){
+    console.log('working');
+    downloadOptions.classList.add('show')
+})
+cancleIcon.addEventListener('click', function(){
+    downloadOptions.classList.remove('show')
+})
+downloadBtn.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        downloadOptions.classList.remove('show')
+    })
+})
+
+// Toggle menu
+document.querySelector('.profile_icon').addEventListener('click', function(){
+    document.querySelector('.profile_links').classList.toggle('show')
+})
+
+document.querySelectorAll('.profile_links a').forEach(function(tag){
+    tag.addEventListener('click', function(e){
+        e.preventDefault()
+        document.querySelector('.profile_links').classList.remove('show')
+    })
+}) 
+
+///////////////////////////////// SHARE FEATURE //////////////////////////////////
+
+const shareBtn = document.querySelector('.share_btn');
+// const shareNote = document.querySelectorAll('.qr_share_del .share_del div')
+shareBtn.addEventListener('click', async function(e){
+    const image = e.target.closest('.dashboard_details').querySelector('.qrcode_section .qrcode_box .generated_code img');
+    const shareNote = e.target.closest('.buttons').querySelector('.share_notification');
+    shareNote.style.opacity = 1;
+    setTimeout(function(){
+        shareNote.style.opacity = 0;
+    }, 3000)
+
+    // console.log(image);
+    // // ***Here is the code for converting "image source" (url) to "Base64".***
+    let url = image.src
+    console.log(url);
+    const getUrlExtension = (url) => {
+      return url
+        .split(/[#?]/)[0]
+        .split(".")
+        .pop()
+        .trim();
+    }
+    
+    const onImageEdit = async (imgUrl) => {
+      const imgExt = getUrlExtension(imgUrl);
+      const response = await fetch(imgUrl);
+      const blob = await response.blob();
+      const fileArray = new File([blob], "search-icon." + imgExt, {
+        type: blob.type,
+      });
+      console.log(fileArray);
+    
+      if (navigator.canShare(fileArray)) {
+        try {
+           await navigator.share({
+            title: 'qrCodeee',
+            files:[fileArray]
+          })
+          console.log('can share');
+        //   output.textContent = 'Shared!'
+        } catch (error) {
+            console.log('cant share');
+        //   output.textContent = `Error: ${error.message}`
+        }
+      } 
+    }
+    onImageEdit(url)
+})
