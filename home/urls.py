6fcf55e08930bd1a718bf35bb164999ac9f7b3f0
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-08-06 17:03:28 +0100

    links url config - landing/learn pages

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from .views import home_page, about_page, learn_page, contact_page
 app_name = 'home'
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
+    path('', home_page, name='home-page'),
     path('about-us/', about_page, name='about-page'),
     path('learn/', learn_page, name='learn-page'),
     path('contact-us/', contact_page, name='contact-page'),
