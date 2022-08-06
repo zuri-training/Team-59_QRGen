de922c8dcd5e8f1ffc45fa270074fda77e6f0f49
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-08-06 01:19:46 +0100

    api app initial commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class QrCode():
+    '''
+        Demo QrCode Model,
+        the one in the 'qrgen' app will be used
+    '''
+    pass
