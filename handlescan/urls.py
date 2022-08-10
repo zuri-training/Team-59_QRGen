f1724a6a72e5780e36e6d72c02616d6d00214ab1
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-08-10 15:59:59 +0100

    removed handlescan and api app

--- handlescan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import dynamic_code_scan, download
-
-app_name = 'handlescan'
-
-urlpatterns = [
-    path('dynamic/<int:code_id>', dynamic_code_scan, name='dynamic'),
-    path('download/<int:file_id>', download, name='download'),
-]
