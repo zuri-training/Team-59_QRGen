870ccf1b6f8dbebdd59d858194fe25a7b91f9d7e
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-08-14 17:56:44 +0100

    Add base.html configuration

--- home/static/home/js/about_us_script.js
+++ home/static/home/js/about_us_script.js
@@ -1,31 +1,3 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        // scroll-up button show/hide
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    // slide-up
-   $('.scroll-up-btn').click(function () {
-       $('body').animate({ scrollTop: 0 });
-       // removing smooth scroll on slide-up button click
-       $('body').css("scrollBehavior", "auto");
-       console.log('maybe');
-   });
-   $('.navbar .nav-links li a').click(function () {
-       // applying again smooth scroll on menu items click
-       $('body').css("scrollBehavior", "smooth");
-   });
-   // toggle menu/navbar
-   $('.menu-btn').click(function () {
-       $('.navbar .nav-links').toggleClass("active");
-       $('.menu-btn i').toggleClass("active");
-       console.log('yes');
-   });
-});
-
 const slides = document.querySelectorAll('.profile');
 const nextBtn = document.querySelector('.nextslide')
 const slider = function () {
--- home/views.py
+++ home/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 
 def home_page(request):
     # extra context if any
-    return render(request, 'home/landing_page.html')
+    return render(request, 'home/landing_page.html', {'title': 'Home'})
     
 def learn_page(request):
     # extra context if any
-    return render(request, 'home/learn_page.html')
+    return render(request, 'home/learn_page.html', {'title': 'Learn'})
     
 def contact_page(request):
     # extra context if any
-    return render(request, 'home/contact_us_page.html')
+    return render(request, 'home/contact_us_page.html', {'title': 'Contact Us'})
     
 def about_page(request):
     # extra context if any
-    return render(request, 'home/about_us_page.html')
+    return render(request, 'home/about_us_page.html', {'title': 'About Us'})
 
 def documentation(request):
     # extra context if any
-    return render(request, 'home/documentation_page.html')
+    return render(request, 'home/documentation_page.html', {'title': 'APU Documentation'})
     
