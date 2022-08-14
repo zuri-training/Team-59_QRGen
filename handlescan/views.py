9c1b713bfd862f72a1af597f0c698aba97adeb78
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-08-14 18:14:48 +0100

    dynamic email hotfix

--- handlescan/views.py
+++ handlescan/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 import os
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 
@@ -6,32 +6,49 @@ from qrgen.models import QrCode, File
 
 # Create your views here.
 
-
 def dynamic_code_scan(request, code_id, *args, **kwargs):
     qrcode = QrCode.objects.get(id=code_id)
 
     # getting the no of scans
-    print(qrcode.scan_count)
+    
     if qrcode.scan_count is not None:
         qrcode.scan_count += 1
-
     else:
         qrcode.scan_count = 1
 
-    print(qrcode.scan_count)
     qrcode.save()
+
     # get the qrcode action_type
     uploads = ['pdf', 'biz', 'img']
 
-    if qrcode.type not in uploads:
+    # if dynamic, these types won't autoredirect, will give us headache instead
+    email_txt = ['eml', 'txt']
+    print('type: ', qrcode.type)
+    if qrcode.action_type not in uploads and qrcode.action_type not in email_txt:
         # get and redirect to the qrcode action_url
+        print('qrcode.type not in uploads and qrcode.type not in email_txt')
         return redirect(qrcode.input_url)
 
     else:
-        # get file_id from code
-        # file = File.objects.get(id=qrcode.file_id)
-        return HttpResponseRedirect('handlescan:download', args=(qrcode.file_id,))
+        if qrcode.action_type == 'eml':
 
+            # open the default mail app and compose main to the input email address
+
+            return render(request, 'email.html', {'email':qrcode.input_url})
+        
+        elif qrcode.action_type == 'txt':
+
+            # for text input
+            # 
+            # return redirect()
+            pass
+            
+        else:
+            # if it was an uploaded file (if qrcode.type in uploads)
+
+            # get file_id from code
+            file = File.objects.get(id=qrcode.file_id)
+            return HttpResponseRedirect('handlescan:download', args=(qrcode.file_id,))
 
 def download(request, pk):
     try:
@@ -39,10 +56,8 @@ def download(request, pk):
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = BASE_DIR + file.file.url
         with open(file_path, 'rb') as fh:
-            response = HttpResponse(
-                fh.read(), content_type="application/adminupload")
-            response['Content-Disposition'] = 'inline;filename=' + \
-                os.path.basename(file_path)
+            response = HttpResponse(fh.read(), content_type="application/adminupload")
+            response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
             return response
     except:
         # return HttpResponse('File does not exit!!!')
